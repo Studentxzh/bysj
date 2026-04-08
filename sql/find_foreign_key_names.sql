2fbf6db1f91538f5e1697401f31a54319379a2bc
Studentxzh <1530700630@qq.com>
2026-04-08 13:58:03 +0800

    Delete sql directory

--- sql/find_foreign_key_names.sql
+++
@@ -1,7 +0,0 @@
--- 在 campus_textbook_recycle 库中执行，查看外键真实名称（迁移脚本里 DROP FOREIGN KEY 要用这些名字）
-SELECT tc.TABLE_NAME, tc.CONSTRAINT_NAME
-FROM information_schema.TABLE_CONSTRAINTS tc
-WHERE tc.TABLE_SCHEMA = DATABASE()
-  AND tc.CONSTRAINT_TYPE = 'FOREIGN KEY'
-  AND tc.TABLE_NAME IN ('textbook', 'message', 'transaction_record')
-ORDER BY tc.TABLE_NAME, tc.CONSTRAINT_NAME;
